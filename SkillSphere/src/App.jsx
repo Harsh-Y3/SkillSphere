c6d48cda884c1754041e710e8f7b435942652f48
Harsh-Y3 <harshy2815@gmail.com>
2025-02-20 18:36:09 +0530

    create reivew section

--- SkillSphere/src/App.jsx
+++ SkillSphere/src/App.jsx
@@ -14,31 +14,61 @@ function App() {
   const [currentIndex, setCurrentIndex] = useState(0);
 
   const [activeReview, setActiveReview] = useState(0);
+   
+ 
+  
 
   const reviews = [
     {
       name: "Consolata Daria",
       role: "Entrepreneur",
-      text: "Until the Ligula Volutpat Nor the Fringe. Everyone's life is ugly. There are no members of my Convallis, Who is always ahead of Lacinia Amet.",
+      text: "Donec volutpat ligula nec ipsum ultrices fringilla. Quisque vitae est turpis. Nulla sodales convallis mi, quis semper ante lacinia sit amet. Maecenas porta velit a justo commodo mattis at et magna. Cras auctor felis erat, sit amet imperdiet metus lacinia in.",
       rating: 5,
       image: "https://randomuser.me/api/portraits/women/44.jpg",
     },
     {
       name: "John Doe",
       role: "Businessman",
-      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ac magna ut dolor cursus fermentum.",
+      text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod rerum quasi eligendi, consequuntur quia possimus tenetur, odio animi earum, blanditiis iusto. Commodi eligendi reiciendis accusamus aliquam excepturi magnam facilis similique.",
       rating: 4,
       image: "https://randomuser.me/api/portraits/men/45.jpg",
     },
     {
       name: "Jane Smith",
       role: "Designer",
-      text: "Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae.",
+      text: "Voluptatum esse illum, est asperiores nostrum porro quasi debitis dolorum, eligendi rerum cupiditate expedita necessitatibus tempore quae nihil aperiam ut quas enim? Modi sint quod, reprehenderit aspernatur natus esse ipsam.",
       rating: 5,
       image: "https://randomuser.me/api/portraits/women/46.jpg",
     },
+    {
+      name: "Johny Smith",
+      role: "Plumber",
+      text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod rerum quasi eligendi, consequuntur quia possimus tenetur, odio animi earum, blanditiis iusto. Commodi eligendi reiciendis accusamus aliquam excepturi magnam facilis similique.",
+      rating: 5,
+      image: "https://randomuser.me/api/portraits/women/47.jpg",
+    },
   ];
 
+
+ 
+
+  useState(() => 
+    {
+      const interval = setInterval(() => 
+      {
+        setActiveReview((prevIndex) =>
+        
+          prevIndex === reviews.length - 1 ? 0 : prevIndex + 1
+        );
+      }, 7000);
+      return () => clearInterval(interval);
+    }, [reviews.length]);
+    
+  
+
+
+
+// about section
   const data = [
     {
       icon: icons.money,
@@ -196,23 +226,23 @@ const handleImgDotClick = (indexImg) => {
         <motion.div className=" absolute inset-0 flex flex-col justify-center items-start px-12 max-w-2xl "
           >
           <motion.p 
-          initial={{ opacity: 0, x: -50 }} whileInView={{ opacity: 1, x: 0 }} transition={{ duration: 1 }} viewport={{ once: true }}
+          initial={{ opacity: 0, x: -50 }} whileInView={{ opacity: 1, x: 0 }} transition={{ duration: 0.5 }} viewport={{ once: true }}
           className="text-sm uppercase tracking-wide">Let's Get To Work</motion.p>
           <motion.h1 
-          initial={{ opacity: 0, x: -50 }} whileInView={{ opacity: 1, x: 0 }} transition={{ duration: 1, delay: 0.2 }} viewport={{ once: true }}
+          initial={{ opacity: 0, x: -50 }} whileInView={{ opacity: 1, x: 0 }} transition={{ duration: 0.5, delay: 0.2 }} viewport={{ once: true }}
           className="text-5xl font-bold mt-2 leading-tight">Honest, Trustworthy, And Does Good Work.</motion.h1>
           <motion.p 
-          initial={{ opacity: 0, x: -50 }} whileInView={{ opacity: 1, x: 0 }} transition={{ duration: 1, delay: 0.3 }} viewport={{ once: true }}
+          initial={{ opacity: 0, x: -50 }} whileInView={{ opacity: 1, x: 0 }} transition={{ duration: 0.5, delay: 0.3 }} viewport={{ once: true }}
           className="mt-4 text-lg text-white">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum soluta quas quisquam minus cupiditate perferendis, voluptatem illum consequuntur itaque quo.</motion.p>
           <motion.div 
-          initial={{ opacity: 0, y: 50 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 1 }} viewport={{ once: true }}
+          initial={{ opacity: 0, y: 50 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.5 }} viewport={{ once: true }}
           className="mt-6 flex space-x-4">
             <button className="bg-blue-100 text-black px-4 py-2 rounded-lg hover:bg-blue-500 hover:text-black transition duration-300">Get A Quote</button>
             <button className="bg-blue-100 text-black hover:bg-blue-500 hover:text-black transition duration-300 px-6 py-3 rounded-lg">Click here</button>
           </motion.div>
         </motion.div>
         </div>
-
+        
       {/* about hu */}
       <div className="flex justify-center bg-blue-100 h-full pt-10 pb-[100px]  rounded-t-none rounded-b-full rounded-r-none rounded-l-none">
       <div className="relative rounded-lg">
@@ -276,14 +306,14 @@ const handleImgDotClick = (indexImg) => {
         <div className='flex justify-center left-10 '>
           {/* leftside content */}
           <motion.div className=" "
-          initial={{ opacity: 0, x: -50 }} whileInView={{ opacity: 1, x: 0 }} transition={{ duration: 1,  }} viewport={{ once: true }}>
+          initial={{ opacity: 0, x: -50 }} whileInView={{ opacity: 1, x: 0 }} transition={{ duration: 0.5,  }} viewport={{ once: true }}>
             {/* heading */}
             <h3 className=''>24/7 hassle-free </h3>
             <h1 className='text-5xl font-bold'>home and businesses  </h1>
             <h1 className='text-5xl font-bold'>installation and Services</h1>
             {/* photo */}
             <motion.div className="!sticky top-0 "
-            initial={{ opacity: 0, x: -50 }} whileInView={{ opacity: 1, x: 0 }} transition={{ duration: 1, delay: 0.3  }} viewport={{ once: true }}>
+            initial={{ opacity: 0, x: -50 }} whileInView={{ opacity: 1, x: 0 }} transition={{ duration: 0.5, delay: 0.3  }} viewport={{ once: true }}>
             <div className="flex flex-col md:flex-row gap-4 p-6 bg-gray-100">
               {/* Left Column */}
               <div className="flex-1 flex flex-col gap-4 mt-5">
@@ -332,15 +362,15 @@ const handleImgDotClick = (indexImg) => {
             <div className=" w-[500px] border-l-4 border-black">
             
               <motion.p className='pl-3'
-              initial={{ opacity: 0, x: 50 }} whileInView={{ opacity: 1, x: 0 }} transition={{ duration: 1,  }} viewport={{ once: true }}
+              initial={{ opacity: 0, x: 50 }} whileInView={{ opacity: 1, x: 0 }} transition={{ duration: 0.5,  }} viewport={{ once: true }}
               >Imperdiet massa tincidunt nunc pulvinar sapien. Sit amet facilisis magna etiam odio mollis mollis. Integer lacus ligula, imperdiet vel massa in, maximus suscipit turpis. Mauris ac risus sed quam semper auctor. Nam tempus volutpat ipsum, non viverra</motion.p>
             </div>
             <motion.div className="pt-20  group-hover:scale-110 trabnsition-all duration-300"
-             initial={{ opacity: 0, x: 50 }} whileInView={{ opacity: 1, x: 0 }} transition={{ duration: 1, delay: 0.3 }} viewport={{ once: true }}
+             initial={{ opacity: 0, x: 50 }} whileInView={{ opacity: 1, x: 0 }} transition={{ duration: 0.5, delay: 0.3 }} viewport={{ once: true }}
             >
               <div className=''>
                 <motion.div 
-                initial={{ opacity: 0, x: 50 }} whileInView={{ opacity: 1, x: 0 }} transition={{ duration: 1, deplay:0.2 }} viewport={{ once: true }}
+                initial={{ opacity: 0, x: 50 }} whileInView={{ opacity: 1, x: 0 }} transition={{ duration: 0.5, deplay:0.2 }} viewport={{ once: true }}
                 className="mt-10 flex hover:">
                   <div className='flex '>
                     <img src={icons.worker} alt="" className='w-[120px] h-[60px]  bg-blue-200 rounded-full  hover:bg-blue-500 hover:scale-110 hover: transition-all duration-300'/>
@@ -356,7 +386,7 @@ const handleImgDotClick = (indexImg) => {
               
               <div className=''>
                 <motion.div 
-                initial={{ opacity: 0, x: 50 }} whileInView={{ opacity: 1, x: 0 }} transition={{ duration: 1, delay: 0.3 }} viewport={{ once: true }}
+                initial={{ opacity: 0, x: 50 }} whileInView={{ opacity: 1, x: 0 }} transition={{ duration: 0.5, delay: 0.3 }} viewport={{ once: true }}
                 className="mt-10 pt-5 flex">
                   <div className='flex'>
                    <img src={icons.docs} alt="" className='w-[120px] h-[60px]   bg-blue-200 rounded-full  hover:bg-blue-500 hover:scale-110 hover: transition-all duration-300'/>
@@ -371,7 +401,7 @@ const handleImgDotClick = (indexImg) => {
               </div>
               <div className=''>
                 <motion.div
-                initial={{ opacity: 0, x: 50 }} whileInView={{ opacity: 1, x: 0 }} transition={{ duration: 1, delay: 0.5 }} viewport={{ once: true }}
+                initial={{ opacity: 0, x: 50 }} whileInView={{ opacity: 1, x: 0 }} transition={{ duration: 0.5, delay: 0.5 }} viewport={{ once: true }}
                 className="mt-10 pt-5 flex">
                   <div className='flex'>
                    <motion.img src={icons.money} alt="" className='w-[120px] h-[60px] bg-blue-200 rounded-full  hover:bg-blue-500 hover:scale-110 hover: transition-all duration-300'/>
@@ -386,7 +416,7 @@ const handleImgDotClick = (indexImg) => {
               </div>
               <div className=''>
                 <motion.div
-                initial={{ opacity: 0, x: 50 }} whileInView={{ opacity: 1, x: 0 }} transition={{ duration: 1, delay: 0.6 }} viewport={{ once: true }}
+                initial={{ opacity: 0, x: 50 }} whileInView={{ opacity: 1, x: 0 }} transition={{ duration: 0.5, delay: 0.6 }} viewport={{ once: true }}
                 className="mt-10 pt-5 flex ">
                   <div className='flex'>
                    <img src={icons.service} alt="" className='w-[120px] h-[60px] bg-blue-200 rounded-full  hover:bg-blue-500 hover:scale-110 hover: transition-all duration-300'/>
@@ -401,7 +431,7 @@ const handleImgDotClick = (indexImg) => {
               </div>
 
              <motion.div
-             initial={{ opacity: 0, translateZ: 50 }} whileInView={{ opacity: 1, translateZ: 0 }} transition={{ duration: 1,  }} viewport={{ once: true }}
+             initial={{ opacity: 0, translateZ: 50 }} whileInView={{ opacity: 1, translateZ: 0 }} transition={{ duration: 0.5,  }} viewport={{ once: true }}
              className="flex pt-20">
               <button className='border rounded-full bg-red-950 w-[122px] h-[50px] text-white text-xl'>Read More</button>
               <div className="pl-[50px] flex">
@@ -815,8 +845,18 @@ const handleImgDotClick = (indexImg) => {
      
       <div className="flex flex-col justify-center items-center bg-white pt-7">
       <div className="pt-7">
-        <h1 className="font-semibold text-3xl text-center">Before & After</h1>
-        <h1 className="font-bold text-5xl mt-5">Amazing Work Results</h1>
+        <motion.h1 
+         initial={{ opacity: 0, y: 50 }}
+         whileInView={{ opacity: 1, y: 0 }}
+         transition={{ duration: 0.5 }}
+         viewport={{ once: true }}
+        className="font-semibold text-3xl text-center">Before & After</motion.h1>
+        <motion.h1 
+         initial={{ opacity: 0, y: 50 }}
+         whileInView={{ opacity: 1, y: 0 }}
+         transition={{ duration: 0.5, delay: 0.3 }}
+         viewport={{ once: true }}
+        className="font-bold text-5xl mt-5">Amazing Work Results</motion.h1>
         <div className=""></div>
       </div>
       <div className="">
@@ -837,7 +877,7 @@ const handleImgDotClick = (indexImg) => {
                 className="w-[800px] flex-shrink-0"
                 initial={{ opacity: 0, y: 50 }}
                 whileInView={{ opacity: 1, y: 0 }}
-                transition={{ duration: 1, delay: 0.4 }}
+                transition={{ duration: 0.5, delay: 0.4 }}
                 viewport={{ once: true }}
               >
                 <div className="relative flex">
@@ -887,7 +927,7 @@ const handleImgDotClick = (indexImg) => {
               }`}
               initial={{ opacity: 0, y: 50 }}
               whileInView={{ opacity: 1, y: 0 }}
-              transition={{ duration: 1 }}
+              transition={{ duration: 0.5 }}
               viewport={{ once: true }}
             ></motion.button>
           ))}
@@ -900,60 +940,102 @@ const handleImgDotClick = (indexImg) => {
       <section className="bg-white relative h-screen w-screen  pt-10">
       <div className=" mx-auto  p-10 h-auto rounded-lg shadow-lg flex flex-col md:flex-row items-center">
         {/* Left Side - Illustration */}
-        <div className="w-full md:w-1/3">
+        <motion.div 
+         initial={{ opacity: 0, x: -50 }}
+         whileInView={{ opacity: 1, x: 0 }}
+         transition={{ duration: 0.5 }}
+         viewport={{ once: true }}
+        className="w-full md:w-1/3">
           <img
-          className='w-[500px]'
+          className='w-[500px] '
            src="https://wdtthemes.kinsta.cloud/mezan/wp-content/uploads/sites/4/2024/02/testimonial-type2.png"   
             alt="Illustration"
           />
-        </div>
+        </motion.div>
 
         {/* Right Side - Testimonial Content */}
-        <div className=" bg-gray-100 rounded-[50px] w-[70%] h-[800px] p-4 ">
-        <h3 className="text-gray-500 uppercase text-sm font-bold">Client Says</h3>
+        <div className=" bg-gray-100 rounded-[50px] w-[80%] h-[800px] p-4 flex flex-col">
+        <motion.h3 
+         initial={{ opacity: 0, x: -50 }}
+         whileInView={{ opacity: 1, x: 0 }}
+         transition={{ duration: 0.5 }}
+         viewport={{ once: true }}
+        className="text-gray-500 uppercase text-sm font-bold">Client Says</motion.h3>
           <div className='flex' >
           
-          <h2 className=" font-semibold text-5xl mr-10">Happy Customers</h2>
-          <div className='pl-[70px] mt-4 border-l-4 border-yellow-500'>Sem viverra aliquet eget sit amet tellus cras. Scelerisque felis imperdiet proin fermentum leo vel orci.Tortor at auctor urna nunc id. Sit amet facilisis magna etiam odio mollis.</div>
+          <motion.h2 
+           initial={{ opacity: 0, y: 50 }}
+           whileInView={{ opacity: 1, y: 0 }}
+           transition={{ duration: 0.5, delay:0.2 }}
+           viewport={{ once: true }}
+          className=" font-semibold text-5xl mr-10">Happy Customers</motion.h2>
+          <motion.div
+           initial={{ opacity: 0, x: 50 }}
+           whileInView={{ opacity: 1, x: 0 }}
+           transition={{ duration: 0.5 }}
+           viewport={{ once: true }}
+          className='pl-[70px] mt-4 border-l-4 border-yellow-500'>Sem viverra aliquet eget sit amet tellus cras. Scelerisque felis imperdiet proin fermentum leo vel orci.Tortor at auctor urna nunc id. Sit amet facilisis magna etiam odio mollis.</motion.div>
           
           </div>
-          <div className="flex pt-8">
+          <div className="flex pt-8 "
+           style={{
+             
+            
+            transition: "transform 0.5s ease-in-out",
+            // width: `${beforeAfter.length * 100}px`,
+          }}
+          >
             {Array(reviews[activeReview].rating).fill().map((_, i) => (
-              <span key={i} className="text-yellow-500 text-lg">★</span>
+              <motion.span 
+              initial={{ opacity: 0, y: 50 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.5, delay: 0.3 }}
+              viewport={{ once: true }}
+              key={i} className="text-yellow-500  text-3xl">★</motion.span>
             ))}
           </div>
           
 
           {/* Profile Section */}
-          <p className="pt-7 text-gray-600 ">{reviews[activeReview].text} Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum voluptatem nihil beatae perspiciatis repellendus dolor quidem magnam, inventore modi totam voluptate, minima quaerat alias culpa fuga laudantium nemo tempore quas dicta accusamus illo id repellat. Itaque, deleniti? Consectetur alias velit dignissimos voluptatibus perspiciatis sapiente? Molestiae, repellendus similique corporis magni, libero quia quidem, quasi laboriosam repudiandae possimus sint laudantium sit fuga voluptas provident cupiditate? Illo omnis maiores vero voluptatum eveniet ullam, nihil fugiat praesentium obcaecati neque perspiciatis qui consectetur optio asperiores, atque commodi enim, tenetur illum! Autem, numquam repellat ducimus iste facere pariatur illum assumenda dignissimos consequuntur. Quibusdam quos nisi doloremque!</p>
-          <div className="mt-4 flex items-center space-x-3">
-            <img
-              src={reviews[activeReview].image}
-              alt={reviews[activeReview].name}
-              className="w-10 h-10 rounded-full"
-            />
-            <div className='pt-7'>
-              <h4 className="font-semibold">{reviews[activeReview].name}</h4>
-              <p className="text-gray-500 text-sm font-bold">{reviews[activeReview].role}</p>
+          <motion.div 
+           initial={{ opacity: 0, z: 50 }}
+           whileInView={{ opacity: 1, z: 0 }}
+           transition={{ duration: 1 }}
+           viewport={{ once: true }}
+          className='flex flex-col'>
+            <p className="pt-7 text-gray-600 text-3xl">{reviews[activeReview].text} </p>
+            <div className="mt-4 flex  items-center space-x-3"
+            
+            >
+              <img
+                src={reviews[activeReview].image}
+                alt={reviews[activeReview].name}
+                className="w-10 h-10 rounded-full"
+              />
+              <div className='pt-7'>
+                <h4 className="font-semibold">{reviews[activeReview].name}</h4>
+                <p className="text-gray-500 text-sm font-bold">{reviews[activeReview].role}</p>
+              </div>
             </div>
-          </div>
+
         
         
           
           {/* previous review button and next review button */}
-          <div className="mt-6 flex space-x-4 pt-7">
+          <div className="mt-6   space-x-4 pt-7">
         {reviews.map((_, index) => (
           <button
             key={index}
             onClick={() => setActiveReview(index)}
-            className={`px-4 py-2 rounded-lg transition-colors duration-300 ${
+            className={`px-4 py-2 rounded-full w-3 transition-colors duration-300 ${
               activeReview === index ? "bg-yellow-300 text-black" : "bg-gray-300 text-gray-700"
             }`}
           >
-            Review {index + 1}
+        
           </button>
         ))}
       </div>
+      </motion.div>
         </div>
         
 
