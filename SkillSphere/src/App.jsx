92124848e1a24fa91f7311be54fe7dbcd83faefc
Harsh-Y3 <harshy2815@gmail.com>
2025-03-06 16:21:44 +0530

    navbar display on all the pages

--- SkillSphere/src/App.jsx
+++ SkillSphere/src/App.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './components/Home'
 import Login from "./components/Login";
+import Header from "./components/Header";
 
 
 export default function App() {
 
   return <BrowserRouter>
+  <Header />
   <Routes>
     <Route path="/" element = {<Home />} />
     <Route path="/login" element = {<Login />} />
