fe36e596cc348e9c3cb21ab57cdad14f7c97998f
Harsh-Y3 <harshy2815@gmail.com>
2025-03-06 16:41:32 +0530

    create backend server

--- SkillSphere/src/components/Home.jsx
+++ SkillSphere/src/components/Home.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef, useState } from 'react'
-import logo from '../assets/Logo.jpg'
+
 import {delay, motion} from 'framer-motion'
 import icons from '../assets/assets'
 import background from '../assets/background-vid-skillsphere.mp4'
@@ -247,32 +247,7 @@ useEffect(() =>
            
           </video>
         </div>
-        <nav className="absolute top-[-50px] left-0 right-0 h-36 flex justify-between items-center p-4 bg-blue-950 bg-opacity-40">
-          <div className="flex pt-10 items-center space-x-2">
-            <div className="text-yellow-500 text-2xl font-bold flex items-center">
-            <img src={logo} alt="Logo" className="w-[250px] h-[150px] border-s-6 rounded-full mix-blend-lighten "/>
-            </div>
-          </div>
-          <ul className="hidden md:flex space-x-6 text-lg gap-10 pt-10" >
-            <li className="hover:text-yellow-500 cursor-pointer">Home</li>
-            <li className="hover:text-yellow-500 cursor-pointer">About Us</li>
-            <li className="hover:text-yellow-500 cursor-pointer">Shop</li>
-            <li className="hover:text-yellow-500 cursor-pointer">Blogs</li>
-            <li className="hover:text-yellow-500 cursor-pointer">Contact</li>
-          </ul>
-          <div
-           className='items-end '>
-          <button
-            
-            className="bg-blue-100 text-black w-20 mr-10 h-10 mt-10 rounded-lg hover:bg-blue-500 hover:text-black transition duration-300"
-           >
-      Login
-    </button>
-          <button className="bg-blue-100 text-black px-4 h-10 mt-10 rounded-lg hover:bg-blue-500 hover:text-black transition duration-300">
-            Get A Quote
-          </button>
-          </div>
-        </nav>
+        
         
         <motion.div className=" absolute inset-0 flex flex-col justify-center items-start px-12 max-w-2xl "
           >
