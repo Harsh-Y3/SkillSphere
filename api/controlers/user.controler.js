f2249516544027374ddfa913e0c0b1c22fda2b0f
Harsh-Y3 <harshy2815@gmail.com>
2025-03-07 13:11:56 +0530

    create signup api route

---
+++ api/controlers/user.controler.js
@@ -0,0 +1,9 @@
+export const test = ('/test', (req, res) => {
+    res.json(
+        {
+         message: 'User Controller Test' 
+        },
+    );  
+
+});
+
--- api/index.js
+++ api/index.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-dotenv.config();
 
+import userRouter from './routes/user.route.js'; 
+import authRouter from './routes/auth.route.js';
+dotenv.config();
 mongoose.connect(process.env.MONGO).then(() => {
     console.log('Connected to MongoDB!');
     }).catch((err) => {
@@ -10,7 +12,12 @@ mongoose.connect(process.env.MONGO).then(() => {
     });
 
 const app = express();
-
+app.use(express.json());
 app.listen(3000, () => {
     console.log('Server is running on 3000!');
-    });
+});
+
+
+app.use('/api/users', userRouter);
+// app.use('/api/users', userRouter);
+app.use('/api/auth', authRouter);
